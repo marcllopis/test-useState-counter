79f66aa43dad0d1393099ece01db2616a834db0c
Marc <marcllopis89@gmail.com>
2022-03-31 12:40:57 +0200

    Adds + button

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import "./App.css";
 
 function App() {
   let [name] = useState("Marc");
+  let [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+
   return (
     <div>
       <h1>State testing</h1>
-      <Header subtitle="Amazing app" />
+      <Header myName={name} subtitle="Amazing app" />
       <h3>HEY! {name} how are you?</h3>
+      <hr />
+      <h1>Counter value: {counter}</h1>
+      <button onClick={increment}>+</button>
+      <button>-</button>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ const Header = (props) => (
   <>
     <h1>This will be a counter!</h1>
     <h2>{props.subtitle}</h2>
+    <p>I know you can send props {props.myName}</p>
   </>
 );
 
